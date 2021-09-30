275f654a3a547da041508a6b62f8cc3623f8e48b
mkamela20 <mkamela20@stuy.edu>
2021-09-30 13:16:26 -0400

    08_hellofxn

--- 07_helloclass/Greet.java
+++ 07_helloclass/Greet.java
@@ -1,5 +1,10 @@
 public class Greet{
 	public static void main(String[] args){
+		greet("It works");
 		System.out.println("No hablo queso");
+	}
+
+	public static void greet(String x) {
+		System.out.println(x);
 	}
 }
