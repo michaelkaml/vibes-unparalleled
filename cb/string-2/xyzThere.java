b2399ac916af1794973428f1b0db3e2551815c0f
Michael Kamela <91078750+michaelkaml@users.noreply.github.com>
2021-11-01 10:52:27 -0400

    Update xyzThere.java

--- cb/string-2/xyzThere.java
+++ cb/string-2/xyzThere.java
@@ -8,6 +8,12 @@ time spent: an excessive amount
 
 String-2 xyzThere
 Return true if the given string contains an appearance of "xyz" where the xyz is not directly preceeded by a period (.). So "xxyz" counts but "x.xyz" does not.
+
+DISCO:
+This one took me an especially long time. The issue was that the final n+=1 was not in an else statement, so under some circumstances it would increment n by 2 instead of 1 like I wanted.
+Only in one of the test cases did this issue manifest, and it took me a while to realize what was wrong.
+QCC:
+I wonder if I could put n+=1 outside all the if and else statements, as every one of them contains n+=1. I believe it might, as I want it to increment n by 1 no matter what, and placing it within the while loop but outside the if and else statments would achieve this.
 */
 
 public boolean xyzThere(String str) {
