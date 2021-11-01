3883262b6b7b50d6a58bbedd4802cba17eea19b8
Michael Kamela <91078750+michaelkaml@users.noreply.github.com>
2021-11-01 10:47:59 -0400

    Update endOther.java

--- cb/string-2/endOther.java
+++ cb/string-2/endOther.java
@@ -8,6 +8,14 @@ time spent: an excessive amount
 
 String-2 endOther
 Given two strings, return true if either of the strings appears at the very end of the other string, ignoring upper/lower case differences (in other words, the computation should not be "case sensitive"). Note: str.toLowerCase() returns the lowercase version of a string.
+
+DISCO:
+By setting the strings to their lowercase versions of themselves, I make the whole process easier by ignoring cases. 
+However, if I had to return one of the string values rather than the boolean, this would present issues.
+I started off by checking if the strings were the same, and then checking which string is longer, and then cutting the two strings down to the same length by removing the beginning of the longer string. 
+Then I check to see if the severed string is the same as the shorter string.
+QCC:
+If I had to return string values, would I just create a second set of strings without the .toLowerCase()?
 */
 
 public boolean endOther(String a, String b) {
