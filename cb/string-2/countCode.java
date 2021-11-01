84557b8073100d7fbfb5ca82a67ecdc1d34b746b
Michael Kamela <91078750+michaelkaml@users.noreply.github.com>
2021-11-01 10:41:09 -0400

    Update countCode.java

--- cb/string-2/countCode.java
+++ cb/string-2/countCode.java
@@ -8,6 +8,12 @@ time spent: an excessive amount
 
 String-2 countCode
 Return the number of times that the string "code" appears anywhere in the given string, except we'll accept any letter for the 'd', so "cope" and "cooe" count.
+
+DISCO:
+By removing the third letter from each time I check for "code", I effectively just check for "co_e", where the "_" represents any character. 
+I also have to do str.length-3 in my while loop to prevent an error, because eventually it would try to check a position on the string that didn't exist. 
+QCC:
+Does n+=1 have to appear in the if statement? Since its present both under if and else, can I just remove the else and have n+=1 within the while loop but outide the if/else?
 */
 
 public int countCode(String str) {
